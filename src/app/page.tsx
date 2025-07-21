614bfd7133a2965796ceefaa3e2fb2c1519200f5
yashu tandon <yashutandon56@gmail.com>
2025-07-22 02:23:46 +0530

    setup TRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+const Page = async() => {
+    const queryClient=getQueryClient();
+    void queryClient.prefetchQuery(trpc.createAi.queryOptions({text:"YashuTandon"}));
   return (
-  <div>
-    Hello , World!
-  </div>
-  );
+  <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<p>loading...</p>}>
+    <Client/>
+    </Suspense>
+  </HydrationBoundary>
+  )
 }
+ 
+export default Page;
