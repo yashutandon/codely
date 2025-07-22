fd6d1545cf61f41c5150508e73feb6d2946ce353
yashu tandon <yashutandon56@gmail.com>
2025-07-22 18:59:15 +0530

    setup inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { trpc, getQueryClient } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+"use client"
 
-const Page = async() => {
-    const queryClient=getQueryClient();
-    void queryClient.prefetchQuery(trpc.createAi.queryOptions({text:"YashuTandon"}));
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+const Page = () => {
+    const trpc= useTRPC();
+    const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
-  <HydrationBoundary state={dehydrate(queryClient)}>
-    <Suspense fallback={<p>loading...</p>}>
-    <Client/>
-    </Suspense>
-  </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={()=>invoke.mutate({text:"Test"})}>
+        Invoke Background Job
+      </Button>
+    </div>
   )
 }
  
