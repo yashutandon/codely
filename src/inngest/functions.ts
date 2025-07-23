314c751a9cb82a37f1f3a0b9b2921a5c3d9f2b59
yashu tandon <yashutandon56@gmail.com>
2025-07-23 19:49:09 +0530

    setup E2B and  install docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
 import {  openai, createAgent } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event}) => {
+  async ({ event,step}) => {
+    const sandboxId=await step.run("get-sandbox-id",async()=>{
+      const sandbox=await Sandbox.create("codely-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
     const codeagent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer.You write readable , maintainable code.You weite simple  Next.js & React snippets.",
@@ -13,7 +19,11 @@ export const helloWorld = inngest.createFunction(
     const {output}= await codeagent.run(
       `Write the following text:${event.data.value}`,
     )
-    console.log(output);
-    return { output };
+    const sandboxUrl=await step.run("get-sandbox-url",async()=>{
+      const sandbox=await getSandbox(sandboxId);
+      const host= sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+    return { output ,sandboxUrl};
   },
 );
